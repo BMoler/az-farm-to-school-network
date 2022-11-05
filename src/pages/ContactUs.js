248eef0008b0da696629f393116145681a9fb711
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2022-11-05 14:51:44 -0700

    Added basic contact us page elements
    also added php mailer class and php for form submit event. Added small changes to index.css for the header. Created a new folder for images

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,81 +1,86 @@
 import { useNavigate } from "react-router-dom";
+import "./ContactUs.css";
 
 function ContactUs() {
-  let navigate = useNavigate();
+  //let navigate = useNavigate();
 
   return (
     <>
-      <h1>CONTACT US PAGE</h1>
-      <button
-        onClick={(e) => {
-          navigate("/about");
-        }}
-      >
-        About
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/account");
-        }}
-      >
-        Account
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/calendar");
-        }}
-      >
-        Calendar
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/contact-us");
-        }}
-      >
-        Contact Us
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/faq");
-        }}
-      >
-        FAQ
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/forum");
-        }}
-      >
-        Forum
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/");
-        }}
-      >
-        Home
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/map");
-        }}
-      >
-        Map
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/resources");
-        }}
-      >
-        Resources
-      </button>
-      <button
-        onClick={(e) => {
-          navigate("/search");
-        }}
-      >
-        Search
-      </button>
+      <div className="hero-img">
+        <h1>Contact Us</h1>
+      </div>
+      <div className="description-section">
+        <div className="description-section-left">
+          <h2>Description goes here</h2>
+          <p>
+            &emsp;Here at AZ Farm to School Network, we want to help you connect
+            with local schools, farmers, and resources to help teach the next
+            generation about sustainability and healthy eating habits.
+            <br />
+            &emsp;Please contact us from the form below for any additional
+            questions. Or find more contact info in the About Us page!
+          </p>
+        </div>
+        <div className="description-section-right" />
+      </div>
+      <div className="form-section">
+        <div className="form-section-left"></div>
+        <div className="form-section-right">
+          <h2>Contact Us</h2>
+          <form action="php/contact.php" method="POST">
+            <div className="form-questions">
+              <div className="form-item">
+                <label for="first-name">First Name</label>
+                <input
+                  aria-required="true"
+                  aria-invalid="true"
+                  type="text"
+                  placeholder="First name"
+                  id="first-name"
+                  name="first_name"
+                  autocomplete="on"
+                />
+              </div>
+              <div className="form-item">
+                <label for="last-name">Last Name</label>
+                <input
+                  aria-required="true"
+                  aria-invalid="true"
+                  type="text"
+                  placeholder="Last name"
+                  id="last-name"
+                  name="last_name"
+                  autocomplete="on"
+                />
+              </div>
+              <div className="form-item">
+                <label for="email">Email</label>
+                <input
+                  aria-required="true"
+                  aria-invalid="true"
+                  type="text"
+                  placeholder="Email"
+                  id="email"
+                  name="email"
+                  autocomplete="on"
+                />
+              </div>
+              <div className="form-item">
+                <label for="questions">Questions &amp; Comments</label>
+                <textarea
+                  aria-required="true"
+                  aria-invalid="true"
+                  id="questions"
+                  name="questions"
+                />
+              </div>
+            </div>
+            <button type="submit" className="button">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
