9fb260f896ea1d12820d59d032ff7e6e16d84d72
Blake Moler <blakemail22701@gmail.com>
2022-11-06 00:49:46 -0700

    Style updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { useState } from "react";
+import { styled } from "@mui/material/styles";
 
 const Navbar = (props) => {
   const [value, setValue] = useState(0);
@@ -9,14 +10,30 @@ const Navbar = (props) => {
     setValue(newValue);
   };
 
+  const StyledTab = styled((props: StyledTabProps) => (
+    <Tab disableRipple {...props} />
+  ))(({ theme }) => ({
+    textTransform: "none",
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    color: "rgba(200, 200, 200, 0.7)",
+    "&.Mui-selected": {
+      color: "#333",
+    },
+    "&.Mui-focusVisible": {
+      backgroundColor: "rgba(100, 95, 228, 0.32)",
+    },
+  }));
+
   return (
     <>
       <Tabs value={value} onChange={handleChange}>
-        <Tab label="Home" value="1" />
-        <Tab label="Resources" value="2" />
-        <Tab label="FAQ" value="3" />
-        <Tab label="About" value="4" />
-        <Tab label="Contact Us" value="5" />
+        <StyledTab label="Home" />
+        <StyledTab label="Resources" />
+        <StyledTab label="FAQ" />
+        <StyledTab label="About" />
+        <StyledTab label="Contact Us" />
       </Tabs>
     </>
   );
