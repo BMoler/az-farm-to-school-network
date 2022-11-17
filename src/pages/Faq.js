bcae39d99df5146c825a5a75ad1d52debbc74321
Harrison Glenn <79805884+hdglenn@users.noreply.github.com>
2022-11-17 10:21:04 -0700

    Basic structure for accordions. TODO: Fix style

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,7 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import "./Faq.css"
 
 function Faq() {
   let navigate = useNavigate();
+
+  const accordionData = {
+    title: 'What are the three pillars of the AZ Farm to School Network?',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+      laborum cupiditate possimus labore, hic temporibus velit dicta earum
+      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+      voluptatem.`
+  };
+
+  const { title, content } = accordionData;
+  const [isActive, setIsActive] = useState(false);
 
   return (
     <>
@@ -93,6 +106,19 @@ function Faq() {
         Contact Us
       </button>
       </div>
+
+      <React.Fragment>
+      <div className="accordion">
+        <div className="accordion-item">
+          <div className="accordion-title"
+            onClick={() => setIsActive(!isActive)}>
+            <div>{title}</div>
+            <div>{isActive ? '-' : '+'}</div>
+          </div>
+        </div>
+        {isActive && <div className="accordion-content">{content}</div>}
+      </div>
+    </React.Fragment>
 
     </>
   );
