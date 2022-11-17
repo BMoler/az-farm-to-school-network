8baed6428a71ac745829dbcef10bf89fb1cd4bf8
Blake Moler <35079152+BMoler@users.noreply.github.com>
2022-11-16 20:04:33 -0700

    Clean up

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,8 @@
 import { Tabs, Tab, AppBar, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
-import { styled } from "@mui/material/styles";
 import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
-  const routes = ["/", "/Resources", "/FAQ", "/About", "/ContactUs"];
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
@@ -12,50 +10,43 @@ const Navbar = (props) => {
   };
 
   return (
-    <>
-      <AppBar position="sticky" color="background">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component="div"
-            color="secondary"
-            className="grow"
-          >
-            AZ Farm To School Network
-          </Typography>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            textColor="secondary"
-            indicatorColor="secondary"
-          >
-            <Tab disableRipple label="Home" component={Link} to="/" />
-            <Tab
-              disableRipple
-              label="Resources"
-              component={Link}
-              to="/resources"
-            />
-            <Tab disableRipple label="FAQ" component={Link} to="/faq" />
-            <Tab disableRipple label="About" component={Link} to="/about" />
-            <Tab
-              disableRipple
-              label="Contact Us"
-              component={Link}
-              to="/contact-us"
-            />
-            <Tab disableRipple label="Forum" component={Link} to="/forum" />
-            <Tab
-              disableRipple
-              label="Calendar"
-              component={Link}
-              to="/calendar"
-            />
-            <Tab disableRipple label="Map" component={Link} to="/map" />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-    </>
+    <AppBar position="sticky" color="background">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          color="secondary"
+          className="grow"
+        >
+          AZ Farm To School Network
+        </Typography>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+        >
+          <Tab disableRipple label="Home" component={Link} to="/" />
+          <Tab
+            disableRipple
+            label="Resources"
+            component={Link}
+            to="/resources"
+          />
+          <Tab disableRipple label="FAQ" component={Link} to="/faq" />
+          <Tab disableRipple label="About" component={Link} to="/about" />
+          <Tab
+            disableRipple
+            label="Contact Us"
+            component={Link}
+            to="/contact-us"
+          />
+          <Tab disableRipple label="Forum" component={Link} to="/forum" />
+          <Tab disableRipple label="Calendar" component={Link} to="/calendar" />
+          <Tab disableRipple label="Map" component={Link} to="/map" />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
   );
 };
 
