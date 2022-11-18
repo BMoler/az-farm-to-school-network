d87e05bd447b463f1d5c4cf611db2e6da6633a88
hhong19 <61360885+hhong19@users.noreply.github.com>
2022-11-18 11:32:49 -0700

    added calendar and font files

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -2,6 +2,7 @@ function Calendar() {
   return (
     <>
       <h1>CALENDAR PAGE</h1>
+      <iframe src="https://calendar.google.com/calendar/embed?src=ds5004n7ab6jhh1il70mljalek%40group.calendar.google.com&ctz=America%2FPhoenix" title="calendar" style={{border: "0"}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </>
   );
 }
