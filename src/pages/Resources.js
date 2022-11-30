0a13511eff90c3c61a178fcea8212c680e345fa0
Blake Moler <35079152+BMoler@users.noreply.github.com>
2022-11-30 16:18:07 -0700

    Sidebar showcase

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -6,6 +6,7 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import Sidebar from "../components/sidebar";
 
 function Resources() {
   const [resourceTab, setResourceTab] = useState(0);
@@ -21,74 +22,80 @@ function Resources() {
   };
 
   return (
-    <div className="m-12">
-      <h1>RESOURCES PAGE</h1>
-      <Tabs
-        value={resourceTab}
-        onChange={handleChange}
-        textColor="primary"
-        indicatorColor="secondary"
-        variant="scrollable"
-        scrollButtons={false}
-        TabIndicatorProps={{
-          sx: {
-            width: "100%",
-            height: "100%",
-            borderRadius: "99999px",
-            opacity: 0.2,
-          },
-        }}
-      >
-        <Tab disableRipple label="Getting Started" />
-        <Tab disableRipple label="Produce" />
-        <Tab disableRipple label="Food Services" />
-        <Tab disableRipple label="School Garden" />
-        <Tab disableRipple label="Farms" />
-        <Tab disableRipple label="Advocacy and Sustainability" />
-        <Tab disableRipple label="Reports and Tools" />
-      </Tabs>
-      <div className="flex justify-between">
-        <div className="w-fit">
-          <h2>F2S Planning Guide</h2>
-          <div className="border-2 border-solid border-light-green">
-            <Document file={f2sPlanning} onLoadSuccess={onDocumentLoadSuccess}>
-              <Page pageNumber={pageNumber} />
-            </Document>
+    <div className="flex">
+      <Sidebar />
+      <div className="m-12">
+        <h1>RESOURCES PAGE</h1>
+        <Tabs
+          value={resourceTab}
+          onChange={handleChange}
+          textColor="primary"
+          indicatorColor="secondary"
+          variant="scrollable"
+          scrollButtons={false}
+          TabIndicatorProps={{
+            sx: {
+              width: "100%",
+              height: "100%",
+              borderRadius: "99999px",
+              opacity: 0.2,
+            },
+          }}
+        >
+          <Tab disableRipple label="Getting Started" />
+          <Tab disableRipple label="Produce" />
+          <Tab disableRipple label="Food Services" />
+          <Tab disableRipple label="School Garden" />
+          <Tab disableRipple label="Farms" />
+          <Tab disableRipple label="Advocacy and Sustainability" />
+          <Tab disableRipple label="Reports and Tools" />
+        </Tabs>
+        <div className="flex">
+          <div className="w-fit mr-20">
+            <h2>F2S Planning Guide</h2>
+            <div className="border-2 border-solid border-light-green">
+              <Document
+                file={f2sPlanning}
+                onLoadSuccess={onDocumentLoadSuccess}
+              >
+                <Page pageNumber={pageNumber} />
+              </Document>
+            </div>
+            <div className="flex justify-between mt-4">
+              <IconButton
+                aria-label="previous page"
+                onClick={() => {
+                  setPageNumber(pageNumber - 1);
+                }}
+              >
+                <ArrowBackIosNewIcon />
+              </IconButton>
+              <IconButton
+                aria-label="next page"
+                onClick={() => {
+                  setPageNumber(pageNumber + 1);
+                }}
+              >
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </div>
           </div>
-          <div className="flex justify-between mt-4">
-            <IconButton
-              aria-label="previous page"
-              onClick={() => {
-                setPageNumber(pageNumber - 1);
-              }}
-            >
-              <ArrowBackIosNewIcon />
-            </IconButton>
-            <IconButton
-              aria-label="next page"
-              onClick={() => {
-                setPageNumber(pageNumber + 1);
-              }}
-            >
-              <ArrowForwardIosIcon />
-            </IconButton>
+          <div className="w-fit mr-20">
+            <h2>SNT F2S Webcast</h2>
+            <iframe
+              src="https://dpi.wi.gov/school-nutrition/training/online-learning#f2s"
+              title="SNT F2S Webcast"
+              className="w-[612px] h-[792px]"
+            />
           </div>
-        </div>
-        <div className="w-fit">
-          <h2>SNT F2S Webcast</h2>
-          <iframe
-            src="https://dpi.wi.gov/school-nutrition/training/online-learning#f2s"
-            title="SNT F2S Webcast"
-            className="w-[612px] h-[792px]"
-          />
-        </div>
-        <div className="w-fit">
-          <h2>Planning Toolkit - Intro to Farm to School</h2>
-          <iframe
-            src="https://www.fns.usda.gov/f2s/planning-toolkit-intro-farm-school-planning-and-building-team"
-            title="Planning Toolkit - Intro to Farm to School"
-            className="w-[612px] h-[792px]"
-          />
+          <div className="w-fit mr-20">
+            <h2>Planning Toolkit - Intro to Farm to School</h2>
+            <iframe
+              src="https://www.fns.usda.gov/f2s/planning-toolkit-intro-farm-school-planning-and-building-team"
+              title="Planning Toolkit - Intro to Farm to School"
+              className="w-[612px] h-[792px]"
+            />
+          </div>
         </div>
       </div>
     </div>
